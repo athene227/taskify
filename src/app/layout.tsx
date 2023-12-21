c08b639227c2192429e8b775345005f053e8af77
athene227 <athene227@gmail.com>
2023-12-21 13:40:47 +0200

    + favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,6 +12,31 @@ export const metadata: Metadata = {
     template: "Taskify | %s",
   },
   description: "Task manager to keep track of your goals",
+  icons: {
+    icon: [
+      {
+        url: "favicon/favicon-32x32.png",
+        type: "image/png",
+        sizes: "32x32",
+      },
+      {
+        url: "favicon/favicon-16x16.png",
+        type: "image/png",
+        sizes: "16x16",
+      },
+    ],
+    apple: {
+      url: "favicon/apple-touch-icon.png",
+      sizes: "180x180",
+    },
+    other: [
+      {
+        rel: "mask-icon",
+        url: "favicon/safari-pinned-tab.svg",
+      },
+    ],
+  },
+  manifest: "favicon/site.webmanifest",
 };
 
 const RootLayout = ({ children }: { children: React.ReactNode }) => {
