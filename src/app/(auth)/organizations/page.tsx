b1cb05e96a25bd21d4eb36a3f9252243dbb618ba
athene227 <athene227@gmail.com>
2024-01-16 18:18:30 +0200

    + navbar to organizations page

--- src/app/(auth)/organizations/page.tsx
+++ src/app/(auth)/organizations/page.tsx
@@ -1,12 +1,18 @@
+import { Navbar } from "@/components/navbar/navbar";
 import { OrganizationList } from "@clerk/nextjs";
 
 const OrganizationsPage = () => {
   return (
-    <OrganizationList
-      hidePersonal
-      afterSelectOrganizationUrl="/organization/:id"
-      afterCreateOrganizationUrl="/organization/:id"
-    />
+    <div className="flex min-h-screen w-screen flex-col">
+      <Navbar />
+      <div className="m-auto">
+        <OrganizationList
+          hidePersonal
+          afterSelectOrganizationUrl="/organization/:id"
+          afterCreateOrganizationUrl="/organization/:id"
+        />
+      </div>
+    </div>
   );
 };
 
